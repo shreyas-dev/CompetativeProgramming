d800d64adf390e74443a5d8ffb1a64ffe5a438f5
Shreyas B1 <shreyas.b1@olacabs.com>
2018-04-28 13:30:59 +0530

    Add FastScanners

---
+++ src/helpers/FasterScanner.java
@@ -0,0 +1,108 @@
+package helpers;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.InputMismatchException;
+class FasterScanner {
+    private InputStream mIs;
+    private byte[] buf = new byte[1024];
+    private int curChar;
+    private int numChars;
+
+    public FasterScanner() {
+        this(System.in);
+    }
+
+    public FasterScanner(InputStream is) {
+        mIs = is;
+    }
+
+    public int read() {
+        if (numChars == -1)
+            throw new InputMismatchException();
+        if (curChar >= numChars) {
+            curChar = 0;
+            try {
+                numChars = mIs.read(buf);
+            } catch (IOException e) {
+                throw new InputMismatchException();
+            }
+            if (numChars <= 0)
+                return -1;
+        }
+        return buf[curChar++];
+    }
+
+    public String nextLine() {
+        int c = read();
+        while (isSpaceChar(c))
+            c = read();
+        StringBuilder res = new StringBuilder();
+        do {
+            res.appendCodePoint(c);
+            c = read();
+        } while (!isEndOfLine(c));
+        return res.toString();
+    }
+
+    public String nextString() {
+        int c = read();
+        while (isSpaceChar(c))
+            c = read();
+        StringBuilder res = new StringBuilder();
+        do {
+            res.appendCodePoint(c);
+            c = read();
+        } while (!isSpaceChar(c));
+        return res.toString();
+    }
+
+    public long nextLong() {
+        int c = read();
+        while (isSpaceChar(c))
+            c = read();
+        int sgn = 1;
+        if (c == '-') {
+            sgn = -1;
+            c = read();
+        }
+        long res = 0;
+        do {
+            if (c < '0' || c > '9')
+                throw new InputMismatchException();
+            res *= 10;
+            res += c - '0';
+            c = read();
+        } while (!isSpaceChar(c));
+        return res * sgn;
+    }
+
+    public int nextInt() {
+        int c = read();
+        while (isSpaceChar(c))
+            c = read();
+        int sgn = 1;
+        if (c == '-') {
+            sgn = -1;
+            c = read();
+        }
+        int res = 0;
+        do {
+            if (c < '0' || c > '9')
+                throw new InputMismatchException();
+            res *= 10;
+            res += c - '0';
+            c = read();
+        } while (!isSpaceChar(c));
+        return res * sgn;
+    }
+
+    public boolean isSpaceChar(int c) {
+        return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+    }
+
+    public boolean isEndOfLine(int c) {
+        return c == '\n' || c == '\r' || c == -1;
+    }
+
+}
--- src/tricks/Tricks.java
+++ src/tricks/Tricks.java
@@ -3,5 +3,5 @@ package tricks;
 public class Tricks {
     //1<<N = 2^N
 
-    //x & (x-1) will give you the last bit
+    //x & (x-1)==0 will tell you whether x is power of 2
 }
