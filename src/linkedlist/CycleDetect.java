296c02236699dc8318ab54b49c687daf0a670692
Rohit-pawar902 <72196393+Rohit-pawar902@users.noreply.github.com>
2020-10-01 18:01:04 +0530

    Update CycleDetect.java

--- src/linkedlist/CycleDetect.java
+++ src/linkedlist/CycleDetect.java
@@ -1,4 +1,23 @@
 package linkedlist;
 
 public class CycleDetect {
+  public boolean hasCycle(ListNode head) {
+        ListNode temp=head;
+        ListNode temp1=head;
+        if(head==null){
+            return false;
+        }
+        else{
+            try{
+        do{
+            temp=temp.next; //move only by one node
+            temp1=temp1.next.next;//move by two node
+        }while(temp!=temp1);
+                return true;
+            }
+            catch(NullPointerException e){
+                return false;
+            }
+            }
+    }
 }
