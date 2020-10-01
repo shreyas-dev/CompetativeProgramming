9d31606b7581f250e2764504a57da6aa9b58e0c7
Rohit-pawar902 <72196393+Rohit-pawar902@users.noreply.github.com>
2020-10-01 17:52:08 +0530

    Update LongestCommonSubsequence.java

--- src/dp/LongestCommonSubsequence.java
+++ src/dp/LongestCommonSubsequence.java
@@ -22,6 +22,28 @@ public class LongestCommonSubsequence {
         }
         return dp[n][m];
     }
+    //recusive solution with dp
+    public static int Longcommonseq(String s1, String s2, int i, int j, int dp[][]) {
+		if (i >= s1.length() || j >= s2.length()) {
+			return 0;
+		}
+
+		if (s1.charAt(i) == s2.charAt(j)) {
+			return 1 + Longcommonseq(s1, s2, i + 1, j + 1, dp);
+		}
+
+		if (dp[i][j] != -1) {
+			return dp[i][j];
+		}
+
+		int a = Longcommonseq(s1, s2, i + 1, j, dp);
+		int b = Longcommonseq(s1, s2, i, j + 1, dp);
+		int c = Longcommonseq(s1, s2, i + 1, j + 1, dp);
+		dp[i][j] = Math.max(a, Math.max(b, c));
+		return dp[i][j];
+
+	}
+    
     public static int longestCommonSubsequence(String a,String b){
         int m=a.length()+1;
         int n=b.length()+1;
